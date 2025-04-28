71bc480161062f932f0d012c7f0ec7591919e820
Romy Tzafrir <romy.tzafrir@mail.huji.ac.il>
2025-04-28 15:41:16 +0300

    Organize TCP server: split into main.go, server.go, and message.go

--- main.go
+++ main.go
@@ -3,83 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-	"net"
 )
-
-type Message struct {
-	from    string
-	payload []byte
-}
-
-type Server struct {
-	listenAddr string
-	ln         net.Listener
-	quitch     chan struct{}
-	msgch      chan Message
-}
-
-func NewServer(listenAddr string) *Server {
-	return &Server{
-		listenAddr: listenAddr,
-		quitch:     make(chan struct{}),
-		msgch:      make(chan Message, 10),
-	}
-}
-
-func (s *Server) Start() error {
-	ln, err := net.Listen("tcp", s.listenAddr) // Start listening for incoming TCP connections
-	if err != nil {
-		return err
-	}
-	defer ln.Close() // Ensure listener is closed when Start exits
-	s.ln = ln
-	go s.acceptLoop()
-	<-s.quitch // Block here until the server is told to quit
-	close(s.msgch)
-	return nil
-}
-
-func (s *Server) acceptLoop() {
-	for {
-		conn, err := s.ln.Accept()
-		if err != nil {
-			fmt.Println("accept error: ", err)
-			continue
-		}
-
-		fmt.Println("new connection to the server: ", conn.RemoteAddr())
-
-		go s.readLoop(conn)
-	}
-}
-
-func (s *Server) readLoop(conn net.Conn) {
-	defer conn.Close() // Make sure to close the connection when done
-	buff := make([]byte, 2048)
-	var msgBuffer []byte
-	for {
-		n, err := conn.Read(buff)
-		if err != nil {
-			fmt.Println("read error: ", err)
-			continue
-		}
-		msgBuffer = append(msgBuffer, buff[:n]...)
-
-		if buff[n-1] == '\n' {
-			s.msgch <- Message{
-				from:    conn.RemoteAddr().String(),
-				payload: msgBuffer,
-			}
-			reply := fmt.Sprintf("\r\n[Server]: Thank you for your message, %s!\r\n", conn.RemoteAddr().String())
-			conn.Write([]byte(reply))
-			msgBuffer = nil
-		}
-	}
-}
 
 func (s *Server) handleMessages() {
 	for msg := range s.msgch {
-		fmt.Printf("received message from connection (%s): %s", msg.from, string(msg.payload))
+		fmt.Printf("Received message from connection (%s): %s", msg.from, string(msg.payload))
 	}
 }
 
@@ -90,6 +18,6 @@ func main() {
 
 	err := server.Start()
 	if err != nil {
-		log.Fatalf("server failed to start: %v", err)
+		log.Fatalf("Server failed to start: %v", err)
 	}
 }
---
+++ message.go
@@ -0,0 +1,6 @@
+package main
+
+type Message struct {
+	from    string
+	payload []byte
+}
