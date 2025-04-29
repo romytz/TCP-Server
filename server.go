d31ef9dedea5683f6e11f8de75921cae21792844
Romy Tzafrir <romy.tzafrir@mail.huji.ac.il>
2025-04-29 17:22:59 +0300

    Add /quit command and track connected clients with a map

--- server.go
+++ server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
+	"sync"
 )
 
 type Server struct {
@@ -10,6 +12,8 @@ type Server struct {
 	ln         net.Listener
 	quitch     chan struct{}
 	msgch      chan Message
+	clients    map[string]net.Conn
+	mu         sync.Mutex
 }
 
 func NewServer(listenAddr string) *Server {
@@ -17,6 +21,7 @@ func NewServer(listenAddr string) *Server {
 		listenAddr: listenAddr,
 		quitch:     make(chan struct{}),
 		msgch:      make(chan Message, 10),
+		clients:    make(map[string]net.Conn),
 	}
 }
 
@@ -47,8 +52,43 @@ func (s *Server) acceptLoop() {
 	}
 }
 
+func (s *Server) registerClient(addr string, conn net.Conn) {
+	s.mu.Lock()
+	s.clients[addr] = conn
+	s.mu.Unlock()
+}
+
+func (s *Server) unregisterClient(addr string) {
+	s.mu.Lock()
+	delete(s.clients, addr)
+	s.mu.Unlock()
+}
+
+func (s *Server) handleMessage(conn net.Conn, msgBuffer []byte, addr string) bool {
+	line := strings.TrimSpace(string(msgBuffer))
+	if line == "/quit" {
+		fmt.Printf("Client requested quit: %s\n", addr)
+		reply := fmt.Sprintf("[Server]: Goodbye, %s!\r\n", conn.RemoteAddr().String())
+		conn.Write([]byte(reply))
+		return true
+	}
+	s.msgch <- Message{
+		from:    conn.RemoteAddr().String(),
+		payload: msgBuffer,
+	}
+	reply := fmt.Sprintf("\r\n[Server]: Thank you for your message, %s!\r\n", conn.RemoteAddr().String())
+	conn.Write([]byte(reply))
+
+	return false
+}
+
 func (s *Server) readLoop(conn net.Conn) {
 	defer conn.Close() // Make sure to close the connection when done
+
+	addr := conn.RemoteAddr().String()
+	s.registerClient(addr, conn)
+	fmt.Printf("Client connected: %s\n", addr)
+
 	buff := make([]byte, 2048)
 	var msgBuffer []byte
 	for {
@@ -60,13 +100,14 @@ func (s *Server) readLoop(conn net.Conn) {
 		msgBuffer = append(msgBuffer, buff[:n]...)
 
 		if buff[n-1] == '\n' {
-			s.msgch <- Message{
-				from:    conn.RemoteAddr().String(),
-				payload: msgBuffer,
+			if s.handleMessage(conn, msgBuffer, addr) {
+				break
 			}
-			reply := fmt.Sprintf("\r\n[Server]: Thank you for your message, %s!\r\n", conn.RemoteAddr().String())
-			conn.Write([]byte(reply))
 			msgBuffer = nil
 		}
 	}
+
+	s.unregisterClient(addr)
+	fmt.Printf("Client disconnected: %s\n", addr)
+
 }
